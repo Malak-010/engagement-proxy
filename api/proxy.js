d83895bcae4344a6b64e33a4aa4090c88f0d83a9
Malak-010 <mallak.ateef@gmail.com>
2025-06-22 20:06:48 +0300

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,21 @@
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  try {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbzXQCQe6TYOLW74i3yk5_AwIW8GwrM5iCkA83BIK3632l1k5PabM_ELSAtmid1P1NqH_Q/exec", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(req.body)
+    });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
+}
